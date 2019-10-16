11aa74f87074776ec380202343b061b6af730d7b
Josiah Swab <josiahjswab@gmail.com>
2019-10-15 22:35:17 -0700

    Fixed syntax bug

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -3,7 +3,7 @@ import rootReducer from './rootReducer';
 
 const rootStore = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX__DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default rootStore;
